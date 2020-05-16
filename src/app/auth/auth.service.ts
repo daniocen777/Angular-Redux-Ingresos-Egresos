d1909df81a28e6b83f6a7de1b031368bd2449cce
Daniel <danielochoa@outlook.com>
2020-05-16 09:41:52 -0500

    Autenticación con firebase - protección de Urls (Guard)

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,22 +1,56 @@
 import { Injectable } from "@angular/core";
+
 import { AngularFireAuth } from "angularfire2/auth";
+import { AngularFirestore } from "angularfire2/firestore";
+import * as firebase from "firebase";
+
 import { Router } from "@angular/router";
+
+import { map } from "rxjs/operators";
+
+import Swal from "sweetalert2";
+
+import { User } from "./user.model";
 
 @Injectable({
   providedIn: "root",
 })
 export class AuthService {
-  constructor(private afAuth: AngularFireAuth, private router: Router) {}
+  constructor(
+    private afAuth: AngularFireAuth,
+    private router: Router,
+    private afDB: AngularFirestore
+  ) {}
+
+  // Escuchar el estado del usuario
+  initAuthListener() {
+    this.afAuth.authState.subscribe((fbUser: firebase.User) => {
+      console.log(fbUser);
+    });
+  }
 
   crearUsuario(nombre: string, email: string, password: string) {
     this.afAuth.auth
       .createUserWithEmailAndPassword(email, password)
       .then((resp) => {
-        // console.log(resp);
-        this.router.navigate(["/"]);
+        // Creanod usuario en la BD
+        const user: User = {
+          uid: resp.user.uid,
+          nombre: nombre,
+          email: resp.user.email,
+        };
+        /* Mandando a firebase */
+        // primer Nivel/Segundo Nivel
+        this.afDB
+          .doc(`${user.uid}/usuario`)
+          .set(user)
+          .then(() => {
+            this.router.navigate(["/"]);
+          });
       })
       .catch((error) => {
-        console.error(error);
+        Swal.fire("Error en el registro", error.message, "error");
+        /* console.error(error); */
       });
   }
 
@@ -28,7 +62,24 @@ export class AuthService {
         this.router.navigate(["/"]);
       })
       .catch((error) => {
-        console.error(error);
+        Swal.fire("Error en el login", error.message, "error");
+        /* console.error(error); */
       });
+  }
+
+  logout() {
+    this.router.navigate(["/login"]);
+    this.afAuth.auth.signOut(); // cerrando sesión
+  }
+
+  isAuth() {
+    return this.afAuth.authState.pipe(
+      map((fbUser) => {
+        if (fbUser == null) {
+          this.router.navigate(["/login"]);
+        }
+        return fbUser != null;
+      })
+    );
   }
 }
--- src/app/shared/sidebar/sidebar.component.ts
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { AuthService } from "../../auth/auth.service";
 
 @Component({
-  selector: 'app-sidebar',
-  templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.css']
+  selector: "app-sidebar",
+  templateUrl: "./sidebar.component.html",
+  styleUrls: ["./sidebar.component.css"],
 })
 export class SidebarComponent implements OnInit {
+  constructor(private authService: AuthService) {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  logout() {
+    this.authService.logout();
   }
-
 }
